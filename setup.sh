b0e635abc74e3e5807ac859f10bfa63a84db6833
Keiran Dennie <keiran.dennie@hart.com>
2020-04-27 12:34:35 -0700

    First

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i -e "s/\(ELASTIC_PASSWORD:\) elasticpass/\1 $ELASTIC_PASS/g" docker-compose.yml
-sed -i -e "s/\(elasticsearch.password:\) elasticpass/\1 $ELASTIC_PASS/g" kibana/config/kibana.yml
-sed -i -e "s/\(xpack.monitoring.elasticsearch.password:\) elasticpass/\1 $ELASTIC_PASS/g" logstash/config/logstash.yml
-sed -i -e "s/\(password =>\) \"elasticpass\"/\1 \"$ELASTIC_PASS\"/g" logstash/pipeline/60-osquery-output.conf
+sed -i "s/\(ELASTIC_PASSWORD:\) elasticpass/\1 $ELASTIC_PASS/g" docker-compose.yml
+sed -i "s/\(elasticsearch.password:\) elasticpass/\1 $ELASTIC_PASS/g" kibana/config/kibana.yml
+sed -i "s/\(xpack.monitoring.elasticsearch.password:\) elasticpass/\1 $ELASTIC_PASS/g" logstash/config/logstash.yml
+sed -i "s/\(password =>\) \"elasticpass\"/\1 \"$ELASTIC_PASS\"/g" logstash/pipeline/60-osquery-output.conf
 
 docker-compose up -d
